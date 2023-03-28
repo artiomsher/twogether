8d5dd4fed876c96430dfd44f4efe38c9fdb8900c
aserstobitov <aser243@gmail.com>
2023-03-28 22:42:36 +0300

    main activity preparation

--- app/src/main/java/com/example/twogether/MainActivity.kt
+++ app/src/main/java/com/example/twogether/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.twogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.twogether.ui.theme.TwogetherTheme
@@ -18,26 +21,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             TwogetherTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android test")
-                }
+                TwogetherApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TwogetherTheme {
-        Greeting("Android")
+fun TwogetherApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Initial screen")
+        }
     }
 }
