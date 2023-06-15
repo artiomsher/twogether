9fa2bf7cadf165850c904e1b307e1d9c24ed041a
aserstobitov <aser243@gmail.com>
2023-06-16 01:20:28 +0300

    toMap to toMutableMap

--- app/src/main/java/com/example/twogether/model/User.kt
+++ app/src/main/java/com/example/twogether/model/User.kt
@@ -1,13 +1,13 @@
 package com.example.twogether.model
 
 data class User(
-     val id: String?,
-     val userId: String,
-     val displayName: String,
-     val uniqueCode: String,
-     val avatarUrl: String
+     val id: String? = "",
+     val userId: String? = "",
+     val displayName: String? = "",
+     val uniqueCode: String? = "",
+     val avatarUrl: String? = ""
 ) {
-    fun toMap(): MutableMap<String, Any> {
+    fun toMutableMap(): MutableMap<String, String?> {
         return mutableMapOf(
             "user_id" to this.userId,
             "display_name" to this.displayName,
