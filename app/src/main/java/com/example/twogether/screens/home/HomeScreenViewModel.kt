e766695f6c1e547ad159d67eb96bc08689d81f82
aserstobitov <aser243@gmail.com>
2024-02-20 11:38:23 +0200

    pairing base added

--- app/src/main/java/com/example/twogether/screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/twogether/screens/home/HomeScreenViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.twogether.screens.home
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -24,27 +25,50 @@ class HomeScreenViewModel @Inject constructor(
 ) : ViewModel() {
     private val auth: FirebaseAuth = Firebase.auth
     val pairPartner: MutableState<DataOrException<User, Boolean, Exception>> = mutableStateOf(
-        DataOrException(null, true, Exception(""))
+        DataOrException(null, false, Exception(""))
     )
 
     val pair: MutableState<DataOrException<Pair, Boolean, Exception>> = mutableStateOf(
-        DataOrException(null, true, Exception(""))
+        DataOrException(null, false, Exception(""))
     )
 
+    val currentUser: MutableState<DataOrException<User, Boolean, Exception>> = mutableStateOf(
+        DataOrException(null, false, Exception(""))
+    )
+
+    var uniqueCode: MutableState<String> = mutableStateOf("")
+
     init {
-        getPartner()
+        getCurrentUser()
+//        getPartner()
     }
+
+    private fun getCurrentUser() {
+        viewModelScope.launch {
+            val currentUserId = auth.currentUser?.uid
+            currentUser.value.loading = true
+            currentUser.value.data =
+                usersRepository.getUserByField(field = "user_id", value = currentUserId!!).data
+            currentUser.value.data?.let {
+                uniqueCode.value = currentUser.value.data!!.uniqueCode.toString()
+                Log.d("UNIQUECODE", "VIEWMODEL: ${uniqueCode.value}")
+                Log.d("CURRENT USER", "VIEWMODEL: $it")
+                currentUser.value.loading = false
+            }
+        }
+    }
+
 
     private fun getPartner() {
 
         viewModelScope.launch {
-            val ownerId = auth.currentUser?.uid
+            val currentUserId = auth.currentUser?.uid
             pair.value.loading = true
-            pair.value.data = pairsRepository.getPairByUserId(ownerId!!).data
+            pair.value.data = pairsRepository.getPairByUserId(currentUserId!!).data
             pair.value.data?.let { pair ->
                 if (pair.status == PairStatus.ACTIVE) {
                     var partnerId = pair.firstUserId
-                    if (partnerId == ownerId) partnerId = pair.secondUserId
+                    if (partnerId == currentUserId) partnerId = pair.secondUserId
                     pairPartner.value.data =
                         usersRepository.getUserByField(field = "user_id", value = partnerId).data
                 }
