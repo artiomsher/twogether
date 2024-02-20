be953bd1a34384868167d90b5f185982b5a7f651
aserstobitov <aser243@gmail.com>
2024-02-20 11:41:50 +0200

    pair request screen added

--- app/src/main/java/com/example/twogether/navigation/TwogetherScreens.kt
+++ app/src/main/java/com/example/twogether/navigation/TwogetherScreens.kt
@@ -3,13 +3,15 @@ package com.example.twogether.navigation
 enum class TwogetherScreens {
     SplashScreen,
     HomeScreen,
-    LoginScreen;
+    LoginScreen,
+    PairRequestScreen;
 
     companion object {
         fun fromRoute(route: String?): TwogetherScreens = when(route?.substringBefore("/")) {
             SplashScreen.name -> SplashScreen
             HomeScreen.name -> HomeScreen
             LoginScreen.name -> LoginScreen
+            PairRequestScreen.name -> PairRequestScreen
             null -> HomeScreen
             else -> throw IllegalArgumentException("Route $route is not recognized")
         }
