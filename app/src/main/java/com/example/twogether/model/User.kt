e67ab74a6c766019814a441b4dc92e0c305a9644
aserstobitov <aser243@gmail.com>
2024-02-20 11:42:30 +0200

    getters setters

--- app/src/main/java/com/example/twogether/model/User.kt
+++ app/src/main/java/com/example/twogether/model/User.kt
@@ -1,11 +1,22 @@
 package com.example.twogether.model
 
+import com.google.firebase.firestore.PropertyName
+
 data class User(
-     val id: String? = "",
-     val userId: String? = "",
-     val displayName: String? = "",
-     val uniqueCode: String? = "",
-     val avatarUrl: String? = ""
+
+     var id: String? = "",
+     @get:PropertyName("user_id")
+     @set:PropertyName("user_id")
+     var userId: String? = "",
+     @get:PropertyName("display_name")
+     @set:PropertyName("display_name")
+     var displayName: String? = "",
+     @get:PropertyName("unique_code")
+     @set:PropertyName("unique_code")
+     var uniqueCode: String? = "",
+     @get:PropertyName("avatar_url")
+     @set:PropertyName("avatar_url")
+     var avatarUrl: String? = ""
 ) {
     fun toMutableMap(): MutableMap<String, String?> {
         return mutableMapOf(
